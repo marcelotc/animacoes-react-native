908aa90df2f995ce9c907d0fb48ad46c6183fd34
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-06-09 21:51:19 -0300

    Animação timing

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
-  Text,
+  Animated,
+  StyleSheet
 } from 'react-native';
 
+//Animated.View
+//Animated.Text
+//Animated.Image
+//Animated.ScrollView
+
 const App = () => {
+  const [ballY, setBallY] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(ballY, {
+      toValue: 500,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start()
+  }, [])
+
   return (
     <>
-      <View>
-        <Text>Animacoes</Text>
+      <View style={styles.container}>
+        <Animated.View style={[
+          styles.ball,
+          { top: ballY }
+        ]}></Animated.View>
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#f00'
+  }
+})
 
 export default App;
